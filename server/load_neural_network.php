d1781c8c098ac2037cb1b7aae19f43ed9d3b8a9c
TodorBalabanov <todor.balabanov@gmail.com>
2016-07-08 15:19:18 +0300

    Project reorganization.

--- server/load_neural_network.php
+++ server/load_neural_network.php
@@ -1,1 +1,23 @@
-// TODO Load the best network.
+<?php
+header('Content-type:application/json;charset=utf-8');
+
+require 'database.php';
+
+/*
+ * Load the best network.
+ */
+if(isset($_POST['load_neural_network'])) {
+	open_my_db();
+  $result = query_my_db( "SELECT `object`, `rating`, `registered` FROM `neural_networks` ORDER BY `rating` ASC LIMIT 1;" );
+
+  $response = "";
+  if($result != false) {
+    $response = array('found' => 'true', 'object' => ''.$result[0][0], 'rating' => ''.$result[0][1], 'registered' => ''.$result[0][2] );
+  } else {
+    $response = array('found' => 'false');
+  }
+  close_my_db();
+
+  echo json_encode($response);
+}
+?>
